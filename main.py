644d0a11d0525367687385d2e33fab5ab132e1cd
Abi <debrah.ab@northeastern.edu>
2026-01-15 19:53:37 +0000

    adds main code

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def is_leap():
-    return True
+def is_leap(year):
+    if year % 100 == 0:
+        if year % 400 == 0:
+            return True
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+    
