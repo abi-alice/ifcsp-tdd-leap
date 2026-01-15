903d5ba380eb0922e7b461909e2ab5e4cc77b544
Abi <debrah.ab@northeastern.edu>
2026-01-15 19:50:11 +0000

    adds main tests

--- test_is_leap.py
+++ test_is_leap.py
@@ -1,4 +1,11 @@
 from main import is_leap
 
-def test_is_leap():
-    assert 2+2 == 4
+def test_is_leap_happy():
+    assert is_leap(2024) == True
+    assert is_leap(1980) == True
+    assert is_leap(1600) == True
+
+def test_is_leap_unhappy():
+    assert is_leap(1500) == False
+    assert is_leap(2006) == False
+    assert is_leap(1473) == False
